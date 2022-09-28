cadb8b33f02e6f3a67175c3e598b3ce3376134ea
tatu.miinalainen@gmail.com <tatu.miinalainen@gmail.com>
2022-09-28 19:16:44 +0300

    js ja json

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 import data from './files/yleiset.json' assert {type: 'json'}
+import interact from 'https://cdn.skypack.dev/interactjs';
+
+const position = { x: 0, y: 0 }
+
+interact('.draggable').draggable({
+  listeners: {
+    start (event) {
+      console.log(event.type, event.target)
+    },
+    move (event) {
+      position.x += event.dx
+      position.y += event.dy
+
+      event.target.style.transform =
+        `translate(${position.x}px, ${position.y}px)`
+    },
+  }
+})
