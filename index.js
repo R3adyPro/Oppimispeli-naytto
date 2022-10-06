631512355aab414f7666bee004e6a25ae2f38597
tatu.miinalainen@gmail.com <tatu.miinalainen@gmail.com>
2022-10-06 13:11:09 +0300

    roskiten ja laatikoille dynaamiset nimet

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import data from './files/yleiset.json' assert {type: 'json'}
 import interact from 'https://cdn.skypack.dev/interactjs';
 
+const roskaLista = []
+const roskaAstiaLista = []
+const roskislista = ["roskis1", "roskis2", "roskis3", "roskis4", "roskis5", "roskis6", "roskis7", "roskis8", "roskis9", "roski10", "roskis11"]
+
 
 interact('.draggable').draggable({
   listeners: {
@@ -24,7 +28,7 @@ function dragMoveListener (event) {
 }
 
 function endEvent (event){
-
+  
 }
 
 interact(".dropzone").dropzone({
@@ -47,3 +51,35 @@ interact('.draggable').draggable({
 })
 
 window.dragMoveListener = dragMoveListener
+
+function addElement(numero){
+  const uusiDiv = document.createElement("div");
+  uusiDiv.setAttribute("id", "drag-" + numero,)
+  uusiDiv.setAttribute("class", "draggable")
+  uusiDiv.setAttribute("data-x", "450");
+  uusiDiv.setAttribute("data-y", "-1200")
+  const content = document.createTextNode(roskaLista[i])
+  uusiDiv.appendChild(content)
+  const nykyinen = document.getElementById(testi)
+
+  document.body.insertBefore(uusiDiv, nykyinen)
+}
+
+//Roskisten nimien vaihto
+
+for(var i=0; i<Object.keys(data.Roskat).length; i++){
+  roskaLista.push(data.Roskat[i].roska)
+  addElement(i)
+  if(!roskaAstiaLista.includes(data.Roskat[i].minne)){
+    roskaAstiaLista.push(data.Roskat[i].minne)
+  }
+}
+for(var i=0; i<roskaAstiaLista.length; i++){
+  try {
+    document.getElementById(roskislista[i]).innerHTML = roskaAstiaLista[i];
+  }
+  catch (error) {
+    continue
+  }
+}
+
